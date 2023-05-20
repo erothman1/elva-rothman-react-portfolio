e1085e32f12e623ad913f437cbafcaedf84d2ec8
Elva Rothman <erothman@tulane.edu>
2023-05-20 15:10:09 -0400

    added comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PortfolioContainer from "./components/PortfolioContainer"
 
+//functional component that returns the PortfolioContainer component
 const App = () => <PortfolioContainer />
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+//import React, JS library to build user interfaces
 import React from "react"
+//import ReactDOM to provide access to DOM-specific methods 
 import ReactDOM from "react-dom"
+
 import App from "./App"
 import "bootstrap/dist/css/bootstrap.min.css"
 
+//renders App component to the DOM on the #root target element 
 ReactDOM.render(<App />, document.getElementById("root"))
