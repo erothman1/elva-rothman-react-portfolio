f7e328c6ce818170655f1519773e120849789bf2
Elva Rothman <erothman@tulane.edu>
2023-05-22 16:36:46 -0400

    styled contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,13 +2,13 @@ import React, { useState } from "react"
 import { validateEmail } from "../utils/helper"
 
 export default function Contact() {
-    const [ name, setName ] = useState("")
-    const [ email, setEmail ] = useState("")
-    const [ message, setMessage ] = useState("")
-    const [ errorMessage, setErrorMessage ] = useState("")
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
 
     const inputChangeHandler = (event) => {
-        const { target } = event 
+        const { target } = event
         const inputType = target.name
         const inputValue = target.value
 
@@ -23,17 +23,17 @@ export default function Contact() {
 
     const blurHandler = (event) => {
         const { value, name } = event.target;
-    
+
         if (name === "name" && value.trim() === "") {
-          setErrorMessage("Oops. Name is required")
-        } else if (name  === "email" && value.trim() === "") {
-          setErrorMessage("Oops. Email is required")
-        } else if (name  === "message" && value.trim() === "") {
-          setErrorMessage("Oops. Message is required")
+            setErrorMessage("Oops. Name is required")
+        } else if (name === "email" && value.trim() === "") {
+            setErrorMessage("Oops. Email is required")
+        } else if (name === "message" && value.trim() === "") {
+            setErrorMessage("Oops. Message is required")
         } else {
-          setErrorMessage("")
+            setErrorMessage("")
         }
-      }
+    }
 
     const formSubmitHandler = (event) => {
         event.preventDefault()
@@ -45,17 +45,18 @@ export default function Contact() {
             return
         }
 
+        //TODO: form still submits if a field is empty
         if (name === "name" && value.trim() === "") {
             setErrorMessage("Oops. Name is required")
             return
         }
 
-        if (name  === "email" && value.trim() === "") {
+        if (name === "email" && value.trim() === "") {
             setErrorMessage("Oops. Email is required")
             return
         }
 
-        if (name  === "message" && value.trim() === "") {
+        if (name === "message" && value.trim() === "") {
             setErrorMessage("Oops. Message is required")
             return
         }
@@ -71,37 +72,52 @@ export default function Contact() {
         <div>
             <h1>Get in touch!</h1>
             <form className="form">
-                <input
-                    value={name}
-                    name="name"
-                    onChange={inputChangeHandler}
-                    onBlur={blurHandler}
-                    type="text"
-                    placeholder="Name"
-                />
-                <input
-                    value={email}
-                    name="email"
-                    onChange={inputChangeHandler}
-                    onBlur={blurHandler}
-                    type="email"
-                    placeholder="Email"
-                />
-                <input
-                    value={message}
-                    name="message"
-                    onChange={inputChangeHandler}
-                    onBlur={blurHandler}
-                    type="text"
-                    placeholder="Message"
-                />
-                <button type="button" onClick={formSubmitHandler}>Submit</button>
-            </form>
-            {errorMessage && (
-                <div>
-                    <p className="error-text">{errorMessage}</p>
+                <div className="mb-3">
+                    <label className="form-label">Name</label>
+                    <input
+                        value={name}
+                        name="name"
+                        onChange={inputChangeHandler}
+                        onBlur={blurHandler}
+                        type="text"
+                        // placeholder="Name"
+                        className="form-control"
+                    />
                 </div>
-            )}
+                <div className="mb-3">
+                    <label className="form-label">Email</label>
+                    <input
+                        value={email}
+                        name="email"
+                        onChange={inputChangeHandler}
+                        onBlur={blurHandler}
+                        type="email"
+                        // placeholder="Email"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Message</label>
+                    <input
+                        value={message}
+                        name="message"
+                        onChange={inputChangeHandler}
+                        onBlur={blurHandler}
+                        type="text"
+                        // placeholder="Message"
+                        className="form-control message"
+                    />
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <div className="submitButton">
+                <button type="button" onClick={formSubmitHandler} className="btn btn-outline-dark">Submit</button>
+                </div>
+            </form>
+
         </div>
     )
 }
