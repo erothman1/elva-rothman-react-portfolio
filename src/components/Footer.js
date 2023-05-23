e983b1997fe9ac54f6feec5f8dc5e54180e8911a
Elva Rothman <erothman@tulane.edu>
2023-05-23 15:49:27 -0400

    styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer({currentPage}) {
     const isFixedPosition = currentPage === "AboutMe" || currentPage === "Contact"
 
     return (
         <footer className={isFixedPosition ? "fixed-footer" : ""}>
-            <a href="https://github.com/erothman1">GitHub</a>
-            <a href="https://www.linkedin.com/in/elva-rothman-91222620a/">LinkedIn</a>
-            <a href="https://twitter.com/elvacodes">Twitter</a>
+            <a className="footerButtons" href="https://github.com/erothman1"><FontAwesomeIcon icon={faGithub} /> GitHub</a>
+            <a className="footerButtons" href="https://www.linkedin.com/in/elva-rothman-91222620a/"><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</a>
+            <a className="footerButtons" href="https://twitter.com/elvacodes"><FontAwesomeIcon icon={faTwitter} /> Twitter</a>
         </footer>
     )
 }
