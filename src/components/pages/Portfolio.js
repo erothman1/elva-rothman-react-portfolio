e4979464331941cf1ee6ff8aefa7ea955b5a0322
Elva Rothman <erothman@tulane.edu>
2023-05-22 21:51:57 -0400

    fixed weirdness with portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -10,13 +10,15 @@ export default function Portfolio() {
                     {projects.map((project) => (
                         <div className="col-md-4" key={project.id}>
                             <div className="card">
-                            <img className="projectImage" src={project.image} class="card-img-top" alt="..."></img>
-                            <div className="card-body">
-                                <h5 className="card-title">{project.name}</h5>
-                                <p className="card-text">{project.tech}</p>
-                                <a href={project.github} className="btn btn-outline-dark">Github</a>
-                                <a href={project.deployed} className="btn btn-outline-dark">Deployed</a>
-                            </div>
+                                <img className="projectImage" src={project.image} class="card-img-top" alt="..."></img>
+                                <div className="card-body">
+                                    <h5 className="card-title">{project.name}</h5>
+                                    <p className="card-text">{project.tech}</p>
+                                    <a href={project.github} className="btn btn-outline-dark">Github</a>
+                                    {project.deployed && (
+                                        <a href={project.deployed} className="btn btn-outline-dark">Deployed</a>
+                                    )}
+                                </div>
                             </div>
                         </div>
                     ))}
